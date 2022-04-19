22725ffb431e45b95168fc919375108b977ccca4
xiongj5 <joan1222@hotmail.com>
2022-04-19 02:35:25 +0000

    Fix indent in yaml. Fix app label in yaml.

--- api/health.go
+++ api/health.go
@@ -1,8 +1,11 @@
 package api
 
-import "cloudnative/srv"
+import (
+	"cloudnative/srv"
+	"net/http"
+)
 
 // functions user need to write
 func HealthCheck(c *srv.Context) {
-
+	c.WriteJson(http.StatusOK, "Healthy")
 }
